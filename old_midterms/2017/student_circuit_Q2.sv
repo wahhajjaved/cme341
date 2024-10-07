f97657f7481dd5dedb8fb367cea89b88917eae5c
Wahhaj Javed <wahhajjaved@gmail.com>
2024-10-07 16:32:57 -0600

    2017 midterm start

---
+++ old_midterms/2017/student_circuit_Q2.sv
@@ -0,0 +1,22 @@
+// This file contains the initial version of the student circuit for
+// the midterm exam preamble.  When working through the 'example exam question'
+// section of the preamble, you are asked to change this module to implement
+// a different circuit.
+
+module student_circuit_Q2 (
+  input wire        clk,
+  input wire        clear,
+  input wire  [7:0] cct_input,
+  output reg  [7:0] cct_output
+);
+
+// initial circuit - should give 16'H622D in testbench for seed 8'HAA
+// (replace with a different circuit as directed in the preamble)
+
+  always @ *
+    if (clear)
+		  cct_output  = 8'H0;
+    else
+      cct_output  = cct_input;
+
+endmodule
