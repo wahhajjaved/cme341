bd458ae667f8799b5c427c4937d7b004ee057f95
Wahhaj Javed <wahhajjaved@gmail.com>
2024-10-07 22:41:35 -0600

    2018 midterm start

---
+++ old_midterms/2018/student_circuit_Q5.sv
@@ -0,0 +1,15 @@
+module student_circuit_Q5 (
+	input wire        clk,
+	input wire        clear,
+	input wire  [7:0] cct_input,
+	output reg  [7:0] cct_output
+);
+
+
+always @ *
+	if (clear)
+		cct_output  = 8'H0;
+	else
+		cct_output  = cct_input;
+
+endmodule
