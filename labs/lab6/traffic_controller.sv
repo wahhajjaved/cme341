85fa8cfb0f9939abbf248d1ce055f210259baedd
Wahhaj Javed <muj975@usask.ca>
2024-12-03 13:31:21 -0600

    lab 6 walk signals implemented. Not compiled. walk_request_waiting not done

--- labs/lab6/traffic_controller.sv
+++ labs/lab6/traffic_controller.sv
@@ -1,41 +1,86 @@
 module traffic_controller (
 	input clk_27,
+	input debug,
+
 	input not_reset,
 	input not_southbound_left_request,
-	input debug,
+	input not_ns_walk_request,
+	input not_ew_walk_request,
+
 	output [6:0] hex7, //northbound
 	output [6:0] hex5, //southbound
 	output [6:0] hex3, //eastbound
 	output [6:0] hex1, //westbound
-	output [6:0] hex0, //counter
-	output ledg2
+	output [6:0] hex0, //debug
+
+	//debug
+	output ledg0, ledg1, ledg2, ledg3
 );
 
 wire clk;
-wire northbound_green;
-wire northbound_amber;
-wire northbound_red;
-wire southbound_green;
-wire southbound_amber;
-wire southbound_red;
-wire southbound_left;
-wire eastbound_green;
-wire eastbound_amber;
-wire eastbound_red;
-wire westbound_green;
-wire westbound_amber;
-wire westbound_red;
+
+wire northbound_green, northbound_amber, northbound_red;
+wire northbound_walk, northbound_flashing_dont_walk, northbound_dont_walk;
+
+wire southbound_green, southbound_amber, southbound_red, southbound_left;
+wire southbound_walk, southbound_flashing_dont_walk, southbound_dont_walk;
+
+wire eastbound_green, eastbound_amber, eastbound_red;
+wire eastbound_walk, eastbound_flashing_dont_walk, eastbound_dont_walk;
+
+wire westbound_green, westbound_amber, westbound_red;
+wire westbound_walk, westbound_flashing_dont_walk, westbound_dont_walk;
 
 
-reg [3:0] counter;
-
-//KEY push buttons are active low so reset needs to be inverted
+//inverted KEY input
 reg reset;
+reg southbound_left_request;
+reg ns_walk_request;
+reg ew_walk_request;
+
+/********************************************************/
+/*						DEBUG	 						*/
+/********************************************************/
+reg [3:0] counter;
+always @ *
+	ledg0 = reset;
+always @ *
+	ledg1 = southbound_left_request;
+always @ *
+	ledg2 = ns_walk_request;
+always @ *
+	ledg3 = ew_walk_request;
+
+
+// always @ (posedge clk)
+// 	if (reset)
+// 		counter = 4'h0;
+// 	else if (counter == 4'hf)
+// 		counter = 4'h0;
+// 	else
+// 		counter = counter + 4'h1;
+
+/********************************************************/
+/*					Invert Input						*/
+/********************************************************/
+
 always @ *
 	reset = ~not_reset;
 
 always @ *
-	ledg2 = reset;
+	southbound_left_request = ~not_southbound_left_request;
+
+always @ *
+	ns_walk_request = ~not_ns_walk_request;
+
+always @ *
+	ew_walk_request = ~not_ew_walk_request;
+
+/********************************************************/
+/*					Instansiations						*/
+/********************************************************/
+//KEY push buttons are active low so reset needs to be inverted
+
 
 clock clock_1(
 	.clk_27(clk_27),
@@ -47,40 +92,58 @@ clock clock_1(
 traffic_controller_fsm traffic_controller_fsm_1 (
 	.clk(clk),
 	.reset(reset),
-	.southbound_left_request(~not_southbound_left_request),
+	.southbound_left_request(southbound_left_request),
+	.walk_request(ns_walk_request || ew_walk_request),
+
 	.northbound_green(northbound_green),
 	.northbound_amber(northbound_amber),
 	.northbound_red(northbound_red),
+	.northbound_walk(northbound_walk),
+	.northbound_flashing_dont_walk(northbound_flashing_dont_walk),
+	.northbound_dont_walk(northbound_dont_walk),
+
 	.southbound_green(southbound_green),
 	.southbound_amber(southbound_amber),
 	.southbound_red(southbound_red),
 	.southbound_left(southbound_left),
+	.southbound_walk(southbound_walk),
+	.southbound_flashing_dont_walk(southbound_flashing_dont_walk),
+	.southbound_dont_walk(southbound_dont_walk),
+
 	.eastbound_green(eastbound_green),
 	.eastbound_amber(eastbound_amber),
 	.eastbound_red(eastbound_red),
+	.eastbound_walk(eastbound_walk),
+	.eastbound_flashing_dont_walk(eastbound_flashing_dont_walk),
+	.eastbound_dont_walk(eastbound_dont_walk),
+
 	.westbound_green(westbound_green),
 	.westbound_amber(westbound_amber),
 	.westbound_red(westbound_red),
+	.westbound_walk(westbound_walk),
+	.westbound_flashing_dont_walk(westbound_flashing_dont_walk),
+	.westbound_dont_walk(westbound_dont_walk),
+
 	.t(counter)
 );
 
 
-hex_display_lights northbound_display (
+hex_display_lights northbound_traffic_light (
 	.colour({1'b0, northbound_green, northbound_amber, northbound_red}),
 	.hex_segments(hex7)
 );
 
-hex_display_lights southbound_display (
+hex_display_lights southbound_traffic_light (
 	.colour({southbound_left, southbound_green, southbound_amber, southbound_red}),
 	.hex_segments(hex5)
 );
 
-hex_display_lights eastbound_display (
+hex_display_lights eastbound_traffic_light (
 	.colour({1'b0, eastbound_green, eastbound_amber, eastbound_red}),
 	.hex_segments(hex3)
 );
 
-hex_display_lights westbound_display (
+hex_display_lights westbound_traffic_light (
 	.colour({1'b0, westbound_green, westbound_amber, westbound_red}),
 	.hex_segments(hex1)
 );
@@ -88,17 +151,23 @@ hex_display_lights westbound_display (
 
 
 
+hex_display_walk northbound_traffic_light (
+	.state({northbound_walk, northbound_flashing_dont_walk, northbound_dont_walk}),
+	.hex_segments(hex6)
+);
 
-// always @ (posedge clk)
-// 	if (reset)
-// 		counter = 4'h0;
-// 	else if (counter == 4'hf)
-// 		counter = 4'h0;
-// 	else
-// 		counter = counter + 4'h1;
+hex_display_walk southbound_traffic_light (
+	.state({southbound_walk, southbound_flashing_dont_walk, southbound_dont_walk}),
+	.hex_segments(hex4)
+);
 
-hex_display_driver c (
-	.hex_digit(counter),
+hex_display_walk eastbound_traffic_light (
+	.state({eastbound_walk, eastbound_flashing_dont_walk, eastbound_dont_walk}),
+	.hex_segments(hex2)
+);
+
+hex_display_walk westbound_traffic_light (
+	.state({westbound_walk, westbound_flashing_dont_walk, westbound_dont_walk}),
 	.hex_segments(hex0)
 );
 
--- labs/lab6/traffic_controller_fsm.sv
+++ labs/lab6/traffic_controller_fsm.sv
@@ -2,52 +2,97 @@ module traffic_controller_fsm (
 	input clk,
 	input reset,
 	input southbound_left_request,
+	input walk_request,
+
 	output northbound_green, northbound_amber, northbound_red,
+	output northbound_walk, northbound_flashing_dont_walk, northbound_dont_walk,
+
 	output southbound_green, southbound_amber, southbound_red, southbound_left,
+	output southbound_walk, southbound_flashing_dont_walk, southbound_dont_walk,
+
 	output eastbound_green, eastbound_amber, eastbound_red,
+	output eastbound_walk, eastbound_flashing_dont_walk, eastbound_dont_walk,
+
 	output westbound_green, westbound_amber, westbound_red,
+	output westbound_walk, westbound_flashing_dont_walk, westbound_dont_walk,
+
 	output [3:0] t
 );
 
+`define TO timer == 6'd1 //timeout
+`define NTO timer != 6'd1 //not timeout
+
 reg [5:0] timer;
 reg southbound_left_request_waiting;
+reg walk_request_waiting;
 
-reg entering_state_1;
-reg entering_state_2;
-reg entering_state_3;
-reg entering_state_4a;
-reg entering_state_4;
-reg entering_state_5;
-reg entering_state_6;
+reg entering_state_1w, entering_state_1fd, entering_state_1d, entering_state_1,
+	entering_state_2, entering_state_3, entering_state_4a, entering_state_4w,
+	entering_state_4fd, entering_state_4d, entering_state_4a, entering_state_4,
+	entering_state_5, entering_state_6;
 
-reg staying_in_state_1;
-reg staying_in_state_2;
-reg staying_in_state_3;
-reg staying_in_state_4a;
-reg staying_in_state_4;
-reg staying_in_state_5;
-reg staying_in_state_6;
+reg staying_in_state_1w, staying_in_state_1fd, staying_in_state_1d, staying_in_state_1,
+	staying_in_state_2, staying_in_state_3, staying_in_state_4a, staying_in_state_4w,
+	staying_in_state_4fd, staying_in_state_4d, staying_in_state_4a, staying_in_state_4,
+	staying_in_state_5, staying_in_state_6;
 
-reg state_1_d;
-reg state_2_d;
-reg state_3_d;
-reg state_4a_d;
-reg state_4_d;
-reg state_5_d;
-reg state_6_d;
+reg state_1w_d, state_1fd_d, state_1d_d, state_1_d,
+	state_2_d, state_3_d, state_4a_d, state_4w_d,
+	state_4fd_d, state_4d_d, state_4a_d, state_4_d,
+	state_5_d, state_6_d;
 
-reg state_1;
-reg state_2;
-reg state_3;
-reg state_4a;
-reg state_4;
-reg state_5;
-reg state_6;
+reg state_1w, state_1fd, state_1d, state_1,
+	state_2, state_3, state_4a, state_4w,
+	state_4fd, state_4d, state_4a, state_4,
+	state_5, state_6;
 
-// timer
+
+/********************************************************/
+/*						DEBUG	 						*/
+/********************************************************/
+always @ *
+	t = {3'b0, southbound_left_request_waiting};
+	// t = {3'b0, reset};
+	// t = timer[3:0];
+
+
+
+/********************************************************/
+/* 						Inputs	 						*/
+/********************************************************/
+always @ *
+	if (reset)
+		southbound_left_request_waiting <= 1'b0;
+	else if (entering_state_4a)
+		southbound_left_request_waiting <= 1'b0;
+	else
+		southbound_left_request_waiting <= southbound_left_request;
+
+
+/*************** walk request ***************/
+
+always @ *
+	if (reset)
+		walk_request_waiting = 1'b0;
+	else
+		walk_request_waiting <= walk_request;
+
+
+
+
+
+/********************************************************/
+/* 						Timer	 						*/
+/********************************************************/
 always @ (posedge clk or posedge reset)
 	if (reset)
 		timer <= 6'd60;
+	else if (entering_state_1w)
+		timer <= 6'd10;
+	else if (entering_state_1fd)
+		timer <= 6'd20;
+	else if (entering_state_1d)
+		timer <= 6'd30;
 	else if (entering_state_1)
 		timer <= 6'd60;
 	else if (entering_state_2)
@@ -56,23 +101,31 @@ always @ (posedge clk or posedge reset)
 		timer <= 6'd2;
 	else if (entering_state_4a)
 		timer <= 6'd20;
+	else if (entering_state_4w)
+		timer <= 6'd10;
+	else if (entering_state_4fd)
+		timer <= 6'd20;
+	else if (entering_state_4d)
+		timer <= 6'd30;
 	else if (entering_state_4)
 		timer <= 6'd60;
 	else if (entering_state_5)
 		timer <= 6'd6;
 	else if (entering_state_6)
 		timer <= 6'd2;
-	else if (timer == 6'd1)
+	else if (`TO)
 		timer <= timer;
 	else
 		timer <= timer - 6'd1;
 
 
 
-always @ *
-	t = {3'b0, southbound_left_request_waiting};
-	// t = {3'b0, reset};
-	// t = timer[3:0];
+
+
+/********************************************************/
+/* 						States	 						*/
+/********************************************************/
+
 
 /*************** State 1 ***************/
 
@@ -83,22 +136,19 @@ always @ (posedge clk or posedge reset)
 	else
 		state_1 <= state_1_d;
 
-
 //logic for entering state 1
 always @ *
-	if (state_6 && timer == 6'd1 )
+	if (state_6 && !walk_request_waiting && `TO)
 		entering_state_1 <= 1'b1;
 	else
 		entering_state_1 <= 1'b0;
 
-
 //logic for staying in state 1
 always @ *
-	if( (state_1 == 1'b1) && (timer != 6'd1) )
+	if (state_1 && `NTO)
 		staying_in_state_1 <= 1'b1;
 	else
 		staying_in_state_1 <= 1'b0;
-
 
 // d-input for state_1 flip/flop
 always @ *
@@ -108,6 +158,109 @@ always @ *
 		state_1_d <= 1'b1;
 	else // not in state 2 on next posedge clk
 		state_1_d <= 1'b0;
+
+
+
+
+/*************** State 1w ***************/
+
+//state 1w flip flop
+always @ (posedge clk or posedge reset)
+	if (reset)
+		state_1w <= 1'b0;
+	else
+		state_1w <= state_1w_d;
+
+//logic for entering state 1w
+always @ *
+	if (state_6 && walk_request_waiting && `TO)
+		entering_state_1w <= 1'b1;
+	else
+		entering_state_1w <= 1'b0;
+
+//logic for staying in state 1w
+always @ *
+	if (state_1w && `NTO)
+		staying_in_state_1w <= 1'b1;
+	else
+		staying_in_state_1w <= 1'b0;
+
+// d-input for state_1w flip/flop
+always @ *
+	if (entering_state_1w)
+		state_1w_d <= 1'b1;
+	else if (staying_in_state_1w)
+		state_1w_d <= 1'b1;
+	else
+		state_1w_d <= 1'b0;
+
+
+
+/*************** State 1fd ***************/
+
+//state 1fd flip flop
+always @ (posedge clk or posedge reset)
+	if (reset)
+		state_1fd <= 1'b0;
+	else
+		state_1fd <= state_1fd_d;
+
+//logic for entering state 1fd
+always @ *
+	if (state_1w && `TO)
+		entering_state_1fd <= 1'b1;
+	else
+		entering_state_1fd <= 1'b0;
+
+//logic for staying in state 1fd
+always @ *
+	if (state_1fd && `NTO)
+		staying_in_state_1fd <= 1'b1;
+	else
+		staying_in_state_1fd <= 1'b0;
+
+// d-input for state_1fd flip/flop
+always @ *
+	if (entering_state_1fd)
+		state_1fd_d <= 1'b1;
+	else if (staying_in_state_1fd)
+		state_1fd_d <= 1'b1;
+	else
+		state_1fd_d <= 1'b0;
+
+
+
+/*************** State 1d ***************/
+
+//state 1d flip flop
+always @ (posedge clk or posedge reset)
+	if (reset)
+		state_1d <= 1'b0;
+	else
+		state_1d <= state_1d_d;
+
+//logic for entering state 1d
+always @ *
+	if (state_1fd && `TO)
+		entering_state_1d <= 1'b1;
+	else
+		entering_state_1d <= 1'b0;
+
+//logic for staying in state 1d
+always @ *
+	if (state_1d && `NTO)
+		staying_in_state_1fd <= 1'b1;
+	else
+		staying_in_state_1fd <= 1'b0;
+
+// d-input for state_1d flip/flop
+always @ *
+	if (entering_state_1d)
+		state_1d_d <= 1'b1;
+	else if (staying_in_state_1fd)
+		state_1d_d <= 1'b1;
+	else
+		state_1d_d <= 1'b0;
 
 
 
@@ -122,19 +275,19 @@ always @ (posedge clk or posedge reset)
 
 //logic for entering state 2
 always @ *
-	if (state_1 && timer == 6'd1 )
+	if (state_1 && `TO)
+		entering_state_2 <= 1'b1;
+	else if (state_1d && `TO)
 		entering_state_2 <= 1'b1;
 	else
 		entering_state_2 <= 1'b0;
 
-
 //logic for staying in state 2
 always @ *
-	if( (state_2 == 1'b1) && (timer != 6'd1) )
+	if (state_2 && `NTO)
 		staying_in_state_2 <= 1'b1;
 	else
 		staying_in_state_2 <= 1'b0;
-
 
 // d-input for state_2 flip/flop
 always @ *
@@ -147,7 +300,6 @@ always @ *
 
 
 
-
 /*************** State 3 ***************/
 
 //state 3 flip flop
@@ -157,22 +309,19 @@ always @ (posedge clk or posedge reset)
 	else
 		state_3 <= state_3_d;
 
-
 //logic for entering state 3
 always @ *
-	if (state_2 && timer == 6'd1)
+	if (state_2 && `TO)
 		entering_state_3 <= 1'b1;
 	else
 		entering_state_3 <= 1'b0;
 
-
 //logic for staying in state 3
 always @ *
-	if( (state_3 == 1'b1) && (timer != 6'd1) )
+	if (state_3 && `NTO)
 		staying_in_state_3 <= 1'b1;
 	else
 		staying_in_state_3 <= 1'b0;
-
 
 // d-input for state_3 flip/flop
 always @ *
@@ -184,16 +333,8 @@ always @ *
 		state_3_d <= 1'b0;
 
 
+
 /*************** State 4a ***************/
-
-always @ *
-	if (reset)
-		southbound_left_request_waiting <= 1'b0;
-	else if (entering_state_4a)
-		southbound_left_request_waiting <= 1'b0;
-	else
-		southbound_left_request_waiting <= southbound_left_request;
-
 
 //state 4a flip flop
 always @ (posedge clk or posedge reset)
@@ -202,22 +343,19 @@ always @ (posedge clk or posedge reset)
 	else
 		state_4a <= state_4a_d;
 
-
 //logic for entering state 4a
 always @ *
-	if (state_3 && southbound_left_request_waiting && timer == 6'd1 )
+	if (state_3 && southbound_left_request_waiting && `TO )
 		entering_state_4a <= 1'b1;
 	else
 		entering_state_4a <= 1'b0;
 
-
 //logic for staying in state 4a
 always @ *
-	if( (state_4a == 1'b1) && (timer != 6'd1) )
+	if (state_4a && `NTO)
 		staying_in_state_4a <= 1'b1;
 	else
 		staying_in_state_4a <= 1'b0;
-
 
 // d-input for state_4a flip/flop
 always @ *
@@ -230,7 +368,6 @@ always @ *
 
 
 
-
 /*************** State 4 ***************/
 
 //state 4 flip flop
@@ -240,24 +377,21 @@ always @ (posedge clk or posedge reset)
 	else
 		state_4 <= state_4_d;
 
-
 //logic for entering state 4
 always @ *
-	if (state_3 && !southbound_left_request_waiting && timer == 6'd1 )
+	if (state_3 && !southbound_left_request_waiting && !walk_request_waiting && `TO )
 		entering_state_4 <= 1'b1;
-	else if (state_4a && timer == 6'd1)
+	else if (state_4a && !walk_request_waiting && `TO)
 		entering_state_4 <= 1'b1;
 	else
 		entering_state_4 <= 1'b0;
 
-
 //logic for staying in state 4
 always @ *
-	if( (state_4 == 1'b1) && (timer != 6'd1) )
+	if (state_4 && `NTO)
 		staying_in_state_4 <= 1'b1;
 	else
 		staying_in_state_4 <= 1'b0;
-
 
 // d-input for state_4 flip/flop
 always @ *
@@ -270,6 +404,109 @@ always @ *
 
 
 
+/*************** State 4w ***************/
+
+//state 4w flip flop
+always @ (posedge clk or posedge reset)
+	if (reset)
+		state_4w <= 1'b0;
+	else
+		state_4w <= state_4w_d;
+
+//logic for entering state 4w
+always @ *
+	if (state_3 && !southbound_left_request_waiting && walk_request_waiting && `TO )
+		entering_state_4w <= 1'b1;
+	else if (state_4a && walk_request_waiting && `TO )
+		entering_state_4w <= 1'b1;
+	else
+		entering_state_4w <= 1'b0;
+
+//logic for staying in state 4w
+always @ *
+	if (state_4w && `NTO)
+		staying_in_state_4w <= 1'b1;
+	else
+		staying_in_state_4w <= 1'b0;
+
+// d-input for state_4w flip/flop
+always @ *
+	if (entering_state_4w) // enter state 4w on next posedge clk
+		state_4w_d <= 1'b1;
+	else if (staying_in_state_4w)
+		state_4w_d <= 1'b1;
+	else // not in state 4w on next posedge clk
+		state_4w_d <= 1'b0;
+
+
+
+/*************** State 4fd ***************/
+
+//state 4fd flip flop
+always @ (posedge clk or posedge reset)
+	if (reset)
+		state_4fd <= 1'b0;
+	else
+		state_4fd <= state_4fd_d;
+
+//logic for entering state 4fd
+always @ *
+	if (state_4w && `TO )
+		entering_state_4fd <= 1'b1;
+	else
+		entering_state_4fd <= 1'b0;
+
+//logic for staying in state 4fd
+always @ *
+	if (state_4fd && `NTO)
+		staying_in_state_4fd <= 1'b1;
+	else
+		staying_in_state_4fd <= 1'b0;
+
+// d-input for state_4fd flip/flop
+always @ *
+	if (entering_state_4fd) // enter state 4fd on next posedge clk
+		state_4fd_d <= 1'b1;
+	else if (staying_in_state_4fd)
+		state_4fd_d <= 1'b1;
+	else // not in state 4fd on next posedge clk
+		state_4fd_d <= 1'b0;
+
+
+
+/*************** State 4d ***************/
+
+//state 4d flip flop
+always @ (posedge clk or posedge reset)
+	if (reset)
+		state_4d <= 1'b0;
+	else
+		state_4d <= state_4d_d;
+
+//logic for entering state 4d
+always @ *
+	if (state_4fd && `TO )
+		entering_state_4d <= 1'b1;
+	else
+		entering_state_4d <= 1'b0;
+
+//logic for staying in state 4d
+always @ *
+	if (state_4d && `NTO)
+		staying_in_state_4d <= 1'b1;
+	else
+		staying_in_state_4d <= 1'b0;
+
+// d-input for state_4d flip/flop
+always @ *
+	if (entering_state_4d) // enter state 4d on next posedge clk
+		state_4d_d <= 1'b1;
+	else if (staying_in_state_4d)
+		state_4d_d <= 1'b1;
+	else // not in state 4d on next posedge clk
+		state_4d_d <= 1'b0;
+
+
 
 /*************** State 5 ***************/
 //state 5 flip flop
@@ -279,22 +516,21 @@ always @ (posedge clk or posedge reset)
 	else
 		state_5 <= state_5_d;
 
-
 //logic for entering state 5
 always @ *
-	if (state_4 && timer == 6'd1 )
+	if (state_4 && `TO )
+		entering_state_5 <= 1'b1;
+	else if (state_4d && `TO )
 		entering_state_5 <= 1'b1;
 	else
 		entering_state_5 <= 1'b0;
 
-
 //logic for staying in state 5
 always @ *
-	if( (state_5 == 1'b1) && (timer != 6'd1) )
+	if (state_5 && `NTO)
 		staying_in_state_5 <= 1'b1;
 	else
 		staying_in_state_5 <= 1'b0;
-
 
 // d-input for state_5 flip/flop
 always @ *
@@ -304,7 +540,6 @@ always @ *
 		state_5_d <= 1'b1;
 	else // not in state 5 on next posedge clk
 		state_5_d <= 1'b0;
-
 
 
 
@@ -319,19 +554,17 @@ always @ (posedge clk or posedge reset)
 
 //logic for entering state 6
 always @ *
-	if (state_5 && timer == 6'd1 )
+	if (state_5 && `TO )
 		entering_state_6 <= 1'b1;
 	else
 		entering_state_6 <= 1'b0;
 
-
 //logic for staying in state 6
 always @ *
-	if( (state_6 == 1'b1) && (timer != 6'd1) )
+	if (state_6 && `NTO)
 		staying_in_state_6 <= 1'b1;
 	else
 		staying_in_state_6 <= 1'b0;
-
 
 // d-input for state_6 flip/flop
 always @ *
@@ -345,16 +578,17 @@ always @ *
 
 
 
-/*************** Outputs ***************/
 
-// northbound_green enabled in state 4
+/********************************************************/
+/* 						Outputs	 						*/
+/********************************************************/
+
+// northbound_green enabled in state 4w 4fd 4d 4
 always @ *
-	if (state_4)
+	if (state_4w || state_4fd || state_4d || state_4)
 		northbound_green = 1'b1;
 	else
 		northbound_green = 1'b0;
-
-
 
 // northbound_amber enabled in state 5
 always @ *
@@ -363,41 +597,38 @@ always @ *
 	else
 		northbound_amber = 1'b0;
 
-
-
-// northbound_red enabled in state 1, 2, 3, 4a, 6
+// northbound_red enabled in state 1w 1fd 1d 1 2 3 4a 6
 always @ *
-	if (state_1 || state_2 || state_3 || state_6 || state_4a)
+	if (state_1w || state_1fd || state_1d || state_1 || state_2 || state_3 || state_4a ||state_6)
 		northbound_red = 1'b1;
 	else
 		northbound_red = 1'b0;
 
 
 
-// southbound_green enabled in state 4
+
+// northbound_walk enabled in state 4w
 always @ *
-	if (state_4)
-		southbound_green = 1'b1;
+	if (state_4w)
+		northbound_walk = 1'b1;
 	else
-		southbound_green = 1'b0;
+		northbound_walk = 1'b0;
 
-
-
-// southbound_amber enabled in state 5
+// northbound_flashing_dont_walk enabled in state 4fd
 always @ *
-	if (state_5)
-		southbound_amber = 1'b1;
+	if (state_4fd)
+		northbound_flashing_dont_walk = 1'b1;
 	else
-		southbound_amber = 1'b0;
+		northbound_flashing_dont_walk = 1'b0;
 
-
-
-// southbound_red enabled in state 1, 2, 3, 6
+// northbound_dont_walk enabled in state 1w 1fd 1d 1 2 3 4a 4d 4 5 6
 always @ *
-	if (state_1 || state_2 || state_3 || state_6)
-		southbound_red = 1'b1;
+	if (state_1w || state_1fd || state_1d || state_1 || state_2 || state_3
+		|| state_4a || state_4d || state_4 || state_5 || state_6)
+		northbound_dont_walk = 1'b1;
 	else
-		southbound_red = 1'b0;
+		northbound_dont_walk = 1'b0;
+
 
 
 
@@ -410,14 +641,62 @@ always @ *
 
 
 
-// eastbound_green enabled in state 1
+
+// southbound_green enabled in state 4w 4fd 4d 4
 always @ *
-	if (state_1)
+	if (state_4w || state_4fd || state_4d || state_4)
+		southbound_green = 1'b1;
+	else
+		southbound_green = 1'b0;
+
+// southbound_amber enabled in state 5
+always @ *
+	if (state_5)
+		southbound_amber = 1'b1;
+	else
+		southbound_amber = 1'b0;
+
+// southbound_red enabled in state 1w 1fd 1d 1 2 3 6
+always @ *
+	if (state_1w || state_1fd || state_1d || state_1 || state_2 || state_3 || state_6)
+		southbound_red = 1'b1;
+	else
+		southbound_red = 1'b0;
+
+
+
+
+// southbound_walk enabled in state 4w
+always @ *
+	if (state_4w)
+		southbound_walk = 1'b1;
+	else
+		southbound_walk = 1'b0;
+
+// southbound_flashing_dont_walk enabled in state 4fd
+always @ *
+	if (state_4fd)
+		southbound_flashing_dont_walk = 1'b1;
+	else
+		southbound_flashing_dont_walk = 1'b0;
+
+// southbound_dont_walk enabled in state 1w 1fd 1d 1 2 3 4a 4d 4 5 6
+always @ *
+	if (state_1w || state_1fd || state_1d || state_1 || state_2 || state_3
+		|| state_4a || state_4d || state_4 || state_5 || state_6)
+		southbound_dont_walk = 1'b1;
+	else
+		southbound_dont_walk = 1'b0;
+
+
+
+
+// eastbound_green enabled in state 1w 1fd 1d 1
+always @ *
+	if (state_1w || state_1fd || state_1d || state_1)
 		eastbound_green = 1'b1;
 	else
 		eastbound_green = 1'b0;
-
-
 
 // eastbound_amber enabled in state 2
 always @ *
@@ -426,28 +705,46 @@ always @ *
 	else
 		eastbound_amber = 1'b0;
 
-
-
-
-// eastbound_red enabled in state 3, 4a, 4, 5, 6
+// eastbound_red enabled in state 3 4a 4w 4fd 4d 4 5 6
 always @ *
-	if (state_3 || state_4a || state_4 || state_5 || state_6)
+	if (state_3 || state_4a || state_4w || state_4fd || state_4d || state_4 || state_5 || state_6)
 		eastbound_red = 1'b1;
 	else
 		eastbound_red = 1'b0;
 
 
 
-
-// westbound_green enabled in state 1
+// eastbound_walk enabled in state 1w
 always @ *
-	if (state_1)
+	if (state_1w)
+		eastbound_walk = 1'b1;
+	else
+		eastbound_walk = 1'b0;
+
+// eastbound_flashing_dont_walk enabled in state 1fd
+always @ *
+	if (state_1fd)
+		eastbound_flashing_dont_walk = 1'b1;
+	else
+		eastbound_flashing_dont_walk = 1'b0;
+
+// eastbound_dont_walk enabled in state 1d 1 2 3 4a 4w 4fd 4d 4 5 6
+always @ *
+	if (state_1d || state_1 || state_2 || state_3 || state_4a || state_4w
+		|| state_4fd || state_4d || state_4 || state_5 || state_6)
+		eastbound_dont_walk = 1'b1;
+	else
+		eastbound_dont_walk = 1'b0;
+
+
+
+
+// westbound_green enabled in state 1w 1fd 1d 1
+always @ *
+	if (state_1w || state_1fd || state_1d || state_1)
 		westbound_green = 1'b1;
 	else
 		westbound_green = 1'b0;
-
-
-
 
 // westbound_amber enabled in state 2
 always @ *
@@ -456,15 +753,37 @@ always @ *
 	else
 		westbound_amber = 1'b0;
 
-
-
-
-// westbound_red enabled in state 3, 4a, 5, 6
+// westbound_red enabled in state 3 4a 4w 4fd 4d 4 5 6
 always @ *
-	if (state_3 || state_4a || state_4 || state_5 || state_6)
+	if (state_3 || state_4a || state_4w || state_4fd ||
+		state_4d || state_4 || state_5 || state_6)
 		westbound_red = 1'b1;
 	else
 		westbound_red = 1'b0;
+
+
+
+// westbound_walk enabled in state 1w
+always @ *
+	if (state_1w)
+		westbound_walk = 1'b1;
+	else
+		westbound_walk = 1'b0;
+
+// westbound_flashing_dont_walk enabled in state 1fd
+always @ *
+	if (state_1fd)
+		westbound_flashing_dont_walk = 1'b1;
+	else
+		westbound_flashing_dont_walk = 1'b0;
+
+// westbound_dont_walk enabled in state 1d 1 2 3 4a 4w 4fd 4d 4 5 6
+always @ *
+	if (state_1d || state_1 || state_2 || state_3 || state_4a || state_4w
+		|| state_4fd || state_4d || state_4 || state_5 || state_6)
+		westbound_dont_walk = 1'b1;
+	else
+		westbound_dont_walk = 1'b0;
 
 
 
