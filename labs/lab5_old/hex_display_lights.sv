cd3afdfef381a5cb84ba2bba089fc6f9673271c0
Wahhaj <wahhaj.javed@usask.ca>
2024-12-02 22:06:07 -0600

    lab5 done

--- labs/lab5_old/hex_display_lights.sv
+++ labs/lab5_old/hex_display_lights.sv
@@ -1,13 +1,12 @@
 module hex_display_lights(
-	input [3:0] colour, //{green, amber, red, left}
+	input [2:0] colour, //{green, amber, red}
 	output [6:0] hex_segments
 );
 
-localparam 	OFF = 4'b0000,
-			RED = 4'b0001,
-			AMBER = 4'b0010,
-			GREEN = 4'b0100,
-			LEFT = 4'b1000;
+localparam 	OFF = 3'b000,
+			RED = 3'b001,
+			AMBER = 3'b010,
+			GREEN = 3'b100;
 
 always @ *
 	//1 is off, 0 is on
@@ -16,7 +15,6 @@ always @ *
 		RED: hex_segments = 7'b1_111_110;
 		AMBER: hex_segments = 7'b0_111_111;
 		GREEN: hex_segments = 7'b1_110_111;
-		LEFT: hex_segments = 7'b0_111_001;
 		default: hex_segments = 7'b0_000_000;
 	endcase
 
